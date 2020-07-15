2de42eb24e6a3d76e6723ad299da3c37e7142c40
Aubyn92 <livstaubyn92@gmail.com>
2020-07-15 12:13:47 +1000

    Factory for post

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
-    title { "MyString" }
-    description { "MyText" }
-    image_url { "MyText" }
+    title { "I love cooking" }
+    description { "My lovely wee Moroccan dish" }
+    image_url { "https://www.google.com/search?q=moroccan+dish&rlz=1C5CHFA_enAU907AU907&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjz34iUjc7qAhVNVisKHTilCe8Q_AUoAXoECBAQAw&biw=705&bih=855" }
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { 
-    Post.new(
-    title: 'I love cooking',
-    description: 'My lovely wee Moroccan dish',
-    image_url: 'https://www.google.com/search?q=moroccan+dish&rlz=1C5CHFA_enAU907AU907&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjz34iUjc7qAhVNVisKHTilCe8Q_AUoAXoECBAQAw&biw=705&bih=855'
-  )
-}
+  subject { build(:post) }
 
   context 'validations' do
     it 'is valid with valid attributes' do
