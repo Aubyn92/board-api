e8c8121da92703a90f7e30cdec46be77d75beb04
Aubyn92 <livstaubyn92@gmail.com>
2020-07-17 15:52:32 +1000

    fixed errors for rendering posts on the page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user, only: [:create, :update, :destroy]
     def index
         posts = Post.all
-        render json: { posts: posts }
+        render json: posts
     end
 
     def create
