6b9466263b3c45a80365ec23f2b0924ae2cd0e5a
Aubyn92 <livstaubyn92@gmail.com>
2020-07-19 15:07:08 +1000

    Created a users controller, added config to the app controller,added new routes and created a status controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: %i[index create update destroy]
+  post "/login", to: "user_token#create"
+  get "/status", to: "status#index"
+  get "/status/user", to: "status#user"
+  post "/sign-up", to: "users#create"
 end
