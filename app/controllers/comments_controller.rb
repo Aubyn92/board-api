a9a5dc5c9e91b472fff6733b7b97a5b4e2de8b6e
Aubyn92 <livstaubyn92@gmail.com>
2020-07-29 13:24:22 +1000

    Removed unused code.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user, only: [:create]
 
-  # def index
-  #   @post = Post.find(params[:post_id])
-  #   posts = @post.comments.all
-  #   render json: comments.as_json(
-  #     only: [:body, :created_at],
-  #     include: { user: { only: [:username] } }
-  #   )
-  # end
-
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
@@ -17,14 +8,8 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     @comment.save
     render json: @comment
-    # redirect_to post_path(@post)
   end
 
-  # def destroy
-  #   @comment.destroy
-  #   render json: {}, status: :no_content
-  # end
- 
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true 
-    # validates :username, presence: true, uniqueness: true
     has_many :posts 
     has_secure_password
     has_many :comments
